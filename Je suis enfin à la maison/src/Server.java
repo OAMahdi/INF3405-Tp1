ca61fd46cf3f3a015c8aed66652522f5d6fb86d9
Mahdi Ourahmoune <mahdi.ourahmoune@gmail.com>
2022-01-27 19:27:49 -0500

    Ajout de commentaires dans Server.java

--- Je suis enfin à la maison/src/Server.java
+++ Je suis enfin à la maison/src/Server.java
@@ -7,33 +7,31 @@ public class Server {
 	private static ServerSocket listener;
 	
 	public static void main(String[] args) throws Exception {
+		//Keep track of the number of clients
 		int clientNumber = 0;
+		
+		//Socket information
 		String serverAdress = "192.168.2.15";
 		int serverPort = 5000;
 		
+		//Initialize Server Socket
 		listener = new ServerSocket();
 		listener.setReuseAddress(true);
 		InetAddress serverIP = InetAddress.getByName(serverAdress);
-		
 		listener.bind(new InetSocketAddress(serverIP, serverPort));
 		
+		//Initial Server Message
 		System.out.format("The Server is running on %s:%d%n", serverAdress, serverPort);
 		
 		try {
-			
+			//Do the thing
 			Socket clientSocket = listener.accept();
 			clientNumber++;
-			DataInputStream input = new DataInputStream(clientSocket.getInputStream());
-			DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+			DataOutputStream output = 
 			
 			String message = input.readUTF();
 			output.writeUTF(message);
 			
-			/*
-			while (true) {
-				new ClientHandler(listener.accept(), clientNumber++).start();
-			}
-			*/
 		} finally {
 			listener.close();
 		}
@@ -42,16 +40,19 @@ public class Server {
 	}
 	
 	
-	
+	//ClientHandler to take care of each individual client
 	private static class ClientHandler extends Thread {
 		
 		private Socket socket;
 		private int clientNumber;
+		private DataInputStream inputStream;
+		private DataOutputStream outputStream;
 		
 		public ClientHandler(Socket socket, int clientNumber) {
-			// TODO Auto-generated constructor stub
 			this.socket = socket;
 			this.clientNumber = clientNumber;
+			this.inputStream = new DataInputStream(socket.getInputStream());
+			this.outputStream = new DataOutputStream(socket.getOutputStream());
 			System.out.println("New connection with client#" + clientNumber + " at " + socket);
 		}
 		
@@ -62,21 +63,15 @@ public class Server {
 			} catch (IOException e) {
 				System.out.println("Error Handling client# " + clientNumber + ": " + e);
 			} finally {
-				/*
+				
 				try {
 					socket.close();
 				} catch (IOException e) {
 					System.out.println("Couldn't close a socket, what's going on?");
 				}
 				System.out.println("Connection with client# " + clientNumber + " closed");
-			}
-			*/
+			}	
 		}
-		
-		
-
-	}
-	
-
-	}
+	}	
+ }
 }
